adc8940b03c74b8f0c5bf39de7b093161b50fbf8
Luis Starlino <luis.guilherme009@gmail.com>
2025-08-29 15:10:54 -0300

    feat(db): finish the standart configuration

---
+++ TicketFlowRabbitMQ.Order.Data/Context/TicketFlowContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketFlowRabbitMQ.Order.Data.Context
+{
+    public class TicketFlowContextFactory : IDesignTimeDbContextFactory<TicketFlowContext>
+    {
+        public TicketFlowContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TicketFlowContext>();
+
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ticketflow;Username=user;Password=password");
+
+            return new TicketFlowContext(optionsBuilder.Options);
+        }
+    }
+}
