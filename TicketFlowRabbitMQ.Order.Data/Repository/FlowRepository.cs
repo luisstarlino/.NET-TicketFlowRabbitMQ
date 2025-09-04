2b1c55cde7703d707a03c980d3273adf65912735
Luis Starlino <luis.guilherme009@gmail.com>
2025-09-04 11:29:17 -0300

    feat(user): start ad and list crud functions for user model

---
+++ TicketFlowRabbitMQ.Order.Data/Repository/FlowRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketFlowRabbitMQ.Order.Data.Context;
+using TicketFlowRabbitMQ.Order.Domain.Interfaces;
+using TicketFlowRabbitMQ.Order.Domain.Models;
+
+namespace TicketFlowRabbitMQ.Order.Data.Repository
+{
+    internal class FlowRepository : IFlowRepository
+    {
+        private readonly TicketFlowContext _ctx;
+
+        public FlowRepository(TicketFlowContext ctx)
+        {
+            _ctx = ctx;
+        }
+        async public Task<Guid> Add(User user)
+        {
+            try
+            {
+                await _ctx.Users.AddAsync(user);
+                await _ctx.SaveChangesAsync();
+                return user.Id;
+            }
+            catch (Exception ex)
+            {
+                return Guid.Empty;
+            }
+        }
+
+        async public Task<IEnumerable<User>> GetAllUsers()
+        {
+            var users = await _ctx.Users.ToListAsync();
+            return users;
+        }
+
+        async public Task<List<User>>? GetByMail(string mail)
+        {
+            return await _ctx.Users.Where(u => u.Email.Equals(mail)).ToListAsync();
+        }
+    }
+}
---
+++ TicketFlowRabbitMQ.Order.Domain/Interfaces/IFlowRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketFlowRabbitMQ.Order.Domain.Models;
+
+namespace TicketFlowRabbitMQ.Order.Domain.Interfaces
+{
+    public interface IFlowRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<Guid> Add(User user);
+        Task<List<User>>? GetByMail(string mail);
+    }
+}
